aa72cb7366f71a7f2bcc3b9ef99faf1a4186ee0b
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-21 17:39:29 +0530

    f

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -17,7 +17,7 @@ const Pagination = () => {
       );
       setData(response.data);
     } catch (error) {
-      alert('Failed to fetch data');
+      alert('failed to fetch data');
     }
   };
 
