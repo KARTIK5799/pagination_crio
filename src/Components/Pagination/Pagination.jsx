093371ace83910aac3403e4287093c6c620c3840
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-21 16:34:37 +0530

    api

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -38,8 +38,8 @@ const Pagination = () => {
   };
 
   return (
-    <div className="max-w-screen-xl mx-auto p-4  text-center">
-      <h1 className="text-2xl font-bold mb-4 text-white" >Employee Data Table</h1>
+    <div className="max-w-screen-xl mx-auto p-4 text-center">
+      <h1 className="text-2xl font-bold mb-4 text-white">Employee Data Table</h1>
       <table className="w-full border-collapse border rounded-lg overflow-hidden bg-white">
         <thead>
           <tr className="bg-gradient-to-r from-purple-500 to-blue-500 text-white">
@@ -65,28 +65,25 @@ const Pagination = () => {
       </table>
 
       <div className="flex justify-center items-center mt-4">
-        {currentPage > 1 ? (
-          <button
-            className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg`}
-            onClick={prevPage}
-          >
-            &lt; Previous
-          </button>
-        ) : (
-          <button
-            className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg opacity-50 `}
-            
-          >
-            &lt; Previous
-          </button>
-        )}
+        {/* Previous button with conditional styling */}
+        <button
+          className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg ${
+            currentPage === 1 && 'opacity-50 cursor-not-allowed'
+          }`}
+          onClick={prevPage}
+        >
+          &lt; Previous
+        </button>
+
+        {/* Page number */}
         <span className="text-lg font-bold mx-4 text-white">{currentPage}</span>
+
+        {/* Next button */}
         <button
           className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg ${
             currentPage === Math.ceil(data.length / itemsPerPage) && 'opacity-50 '
           }`}
           onClick={nextPage}
-          
         >
           Next &gt;
         </button>
