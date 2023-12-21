68c132197f48d84387dc04232dfc4959fa9447f8
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-21 18:11:46 +0530

    gh

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -17,7 +17,7 @@ const Pagination = () => {
       );
       setData(response.data);
     } catch (error) {
-      alert('Failed to fetch data');
+      alert('failed to fetch data');
     }
   };
 
@@ -38,7 +38,7 @@ const Pagination = () => {
   };
 
   return (
-    <div className="max-w-screen-xl mx-auto p-4 bg-gray-200">
+    <div className="max-w-screen-xl mx-auto p-4 bg-gray-200 text-center">
       <h1 className="text-2xl font-bold mb-4">Employee Data Table</h1>
       <table className="w-full border-collapse border rounded-lg overflow-hidden bg-white">
         <thead>
@@ -66,21 +66,21 @@ const Pagination = () => {
 
       <div className="flex justify-center items-center mt-4">
         <button
-          className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-4 py-2 rounded-lg ${
+          className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg ${
             currentPage === 1 && 'opacity-50 cursor-not-allowed'
           }`}
           onClick={prevPage}
-         
+          disabled={currentPage === 1}
         >
           &lt; Previous
         </button>
         <span className="text-lg font-bold mx-4">{currentPage}</span>
         <button
-          className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-4 py-2 rounded-lg ${
+          className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg ${
             currentPage === Math.ceil(data.length / itemsPerPage) && 'opacity-50 cursor-not-allowed'
           }`}
           onClick={nextPage}
-         
+          disabled={currentPage === Math.ceil(data.length / itemsPerPage)}
         >
           Next &gt;
         </button>
