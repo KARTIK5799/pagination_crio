66c1b7c92a94cebea6341f147ccd4e7031370bd0
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-21 17:38:42 +0530

    cg

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Pagination = () => {
   const [data, setData] = useState([]);
@@ -13,11 +13,11 @@ const Pagination = () => {
   const fetchData = async () => {
     try {
       const response = await axios.get(
-        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
       );
       setData(response.data);
     } catch (error) {
-      alert("Failed to fetch data");
+      alert('Failed to fetch data');
     }
   };
 
@@ -39,9 +39,7 @@ const Pagination = () => {
 
   return (
     <div className="max-w-screen-xl mx-auto p-4 text-center">
-      <h1 className="text-2xl font-bold mb-4 text-white">
-        Employee Data Table
-      </h1>
+      <h1 className="text-2xl font-bold mb-4 text-white">Employee Data Table</h1>
       <table className="w-full border-collapse border rounded-lg overflow-hidden bg-white">
         <thead>
           <tr className="bg-gradient-to-r from-purple-500 to-blue-500 text-white">
@@ -67,31 +65,33 @@ const Pagination = () => {
       </table>
 
       <div className="flex justify-center items-center mt-4">
+  {/* Previous button with reduced opacity on the first page */}
+  <button
+    className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg ${
+      currentPage === 1 ? 'opacity-50' : ''
+    }`}
+    onClick={prevPage}
+  >
+    &lt; Previous
+  </button>
+
+  {/* Page number */}
+  <span className="text-lg font-bold mx-4">{currentPage}</span>
+
+  {/* Next button with conditional rendering */}
+  {currentPage < Math.ceil(data.length / itemsPerPage) && (
+    <button
+      className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg`}
       
-        <button
-          className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg ${
-            currentPage === 1 ? "opacity-50" : ""
-          }`}
-          onClick={prevPage}
-        >
-          &lt; Previous
-        </button>
 
-     
-        <span className="text-lg font-bold mx-4">{currentPage}</span>
+      disabled={currentPage === Math.ceil(data.length / itemsPerPage)}
+      onClick={nextPage}
+    >
+      Next &gt;
+    </button>
+  )}
+</div>
 
-     
-        <button
-          className={`bg-gradient-to-r from-purple-500 to-blue-500 text-white px-6 py-2 rounded-lg ${
-            currentPage === Math.ceil(data.length / itemsPerPage)
-              ? "opacity-50"
-              : ""
-          }`}
-          onClick={nextPage}
-        >
-          Next &gt;
-        </button>
-      </div>
     </div>
   );
 };
